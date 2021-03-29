7e8cb2a366e3d664817404bd54949b55e0d41939
Вдовенко Віталій <taybenberg@gmail.com>
2021-03-29 20:05:44 +0300

    GetColor uint values

--- ServitorDiscordBot/GetMessageColor.cs
+++ ServitorDiscordBot/GetMessageColor.cs
@@ -1,37 +1,32 @@
-﻿using System.Drawing;
+﻿using Discord;
 using static ServitorDiscordBot.MessageColors;
 
 namespace ServitorDiscordBot
 {
     public partial class ServitorBot
     {
-        Discord.Color? GetColor(MessageColors colors)
+        Color GetColor(MessageColors colors) => colors switch
         {
-            Color? color = colors switch
-            {
-                Error => ColorTranslator.FromHtml("#d50000"),
-                BumpNotification => ColorTranslator.FromHtml("#3949ab"),
-                Bumped => ColorTranslator.FromHtml("#6f74dd"),
-                Bip => ColorTranslator.FromHtml("#ae52d4"),
-                Help => ColorTranslator.FromHtml("#7c43bd"),
-                Modes => ColorTranslator.FromHtml("#63a4ff"),
-                ClanActivities => ColorTranslator.FromHtml("#e4e65e"),
-                MyActivities => ColorTranslator.FromHtml("#ffca28"),
-                MyPartners => ColorTranslator.FromHtml("#82e9de"),
-                NotRegistered => ColorTranslator.FromHtml("#fb8c00"),
-                AlreadyRegistered => ColorTranslator.FromHtml("#ffdd71"),
-                Register => ColorTranslator.FromHtml("#81d4fa"),
-                RegisterSuccessful => ColorTranslator.FromHtml("#76ff03"),
-                RegisterNeedMoreInfo => ColorTranslator.FromHtml("#ff7043"),
-                Suspicious => ColorTranslator.FromHtml("#546e7a"),
-                Xur => ColorTranslator.FromHtml("#e0f7fa"),
-                Eververse => ColorTranslator.FromHtml("#ffffcf"),
-                ClanStats => ColorTranslator.FromHtml("#76d275"),
-                Leaderboard => ColorTranslator.FromHtml("#2962ff"),
-                _ => ColorTranslator.FromHtml("#fafafa")
-            };
-
-            return (Discord.Color?)color;
-        }
+            Error => new Color(0xd50000),
+            BumpNotification => new Color(0x3949ab),
+            Bumped => new Color(0x6f74dd),
+            Bip => new Color(0xae52d4),
+            Help => new Color(0x7c43bd),
+            Modes => new Color(0x63a4ff),
+            ClanActivities => new Color(0xe4e65e),
+            MyActivities => new Color(0xffca28),
+            MyPartners => new Color(0x82e9de),
+            NotRegistered => new Color(0xfb8c00),
+            AlreadyRegistered => new Color(0xffdd71),
+            Register => new Color(0x81d4fa),
+            RegisterSuccessful => new Color(0x76ff03),
+            RegisterNeedMoreInfo => new Color(0xff7043),
+            Suspicious => new Color(0x546e7a),
+            Xur => new Color(0xe0f7fa),
+            Eververse => new Color(0xffffcf),
+            ClanStats => new Color(0x76d275),
+            Leaderboard => new Color(0x2962ff),
+            _ => new Color(0xfafafa)
+        };
     }
 }
