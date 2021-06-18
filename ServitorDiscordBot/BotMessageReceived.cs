ca205f11409b8b7193217f78dfd84fbaf2bee2e1
Вдовенко Віталій <taybenberg@gmail.com>
2021-06-18 19:55:36 +0300

    Delete command

--- ServitorDiscordBot/BotMessageReceived.cs
+++ ServitorDiscordBot/BotMessageReceived.cs
@@ -1,4 +1,5 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,35 +10,88 @@ namespace ServitorDiscordBot
     {
         private async Task MessageReceivedAsync(SocketMessage message)
         {
+            if (message.Channel.Id == _bumpChannelId && message.Author.IsBot && message.Embeds.Count > 0)
+            {
+                await InitBumpAsync(message);
+
+                return;
+            }
+
+            if (message.Author.IsBot)
+                return;
+
             var command = message?.Content?.ToLower();
 
             switch (command)
             {
                 case "!my_id":
                     var mid = await message.Channel.SendMessageAsync(message.Author.Id.ToString());
-                    
-                    await message.DeleteAsync();
-                    
+
+                    try
+                    {
+                        await message.DeleteAsync();
+                    }
+                    catch (Exception) { }
+
                     await Task.Delay(5000);
-                    
+
                     await mid.DeleteAsync();
                     return;
 
                 case "!channel_id":
                     var cid = await message.Channel.SendMessageAsync(message.Channel.Id.ToString());
-                    
-                    await message.DeleteAsync();
+
+                    try
+                    {
+                        await message.DeleteAsync();
+                    }
+                    catch (Exception) { }
 
                     await Task.Delay(5000);
 
                     await cid.DeleteAsync();
                     return;
+
+                case string c when c.StartsWith("!delete"):
+                    if (!((SocketGuildUser)message.Author).Roles.Any(x => x.Name.ToLower().StartsWith("admin") || x.Name.ToLower().StartsWith("moder")))
+                    {
+                        await message.Channel.SendMessageAsync($"У вас відсутні права на видалення повідомлень.");
+                        return;
+                    }
+
+                    var strs = c.Split('/');
+
+                    if (strs.Length < 4)
+                        return;
+
+                    try
+                    {
+                        var chid = ulong.Parse(strs[^2]);
+                        var msid = ulong.Parse(strs[^1]);
+
+                        var ch = _client.GetChannel(chid) as IMessageChannel;
+                        var ms = await ch.GetMessageAsync(msid);
+
+                        if (ms is not null)
+                        {
+                            var messages = await ch.GetMessagesAsync(ms, Direction.After).Flatten().ToListAsync();
+
+                            var notification = await message.Channel.SendMessageAsync($"Чистка {messages.Count} повідомлень...");
+
+                            foreach (var m in messages)
+                            {
+                                await m.DeleteAsync();
+                            }
+
+                            await notification.DeleteAsync();
+                        }
+                    }
+                    catch (Exception) { }
+
+                    return;
             }
 
-            if (message.Channel.Id == _bumpChannelId && message.Author.IsBot && message.Embeds.Count > 0)
-                await InitBumpAsync(message);
-
-            if (message.Author.Id == _client.CurrentUser.Id || message.Author.IsBot || !_channelId.Any(x => x == message.Channel.Id))
+            if (message.Author.Id == _client.CurrentUser.Id || !_channelId.Any(x => x == message.Channel.Id))
                 return;
 
             switch (command)
