4ed836adf4d9269535d2bf19f0013bb657bc3a9d
Віталій Вдовенко <taybenberg@gmail.com>
2021-08-09 12:44:16 +0300

    Code update

--- DataProcessor/DiscordEmoji/ServiceEmoji.cs
+++ DataProcessor/DiscordEmoji/ServiceEmoji.cs
@@ -5,5 +5,9 @@
         public static readonly string Loading = "<a:ServitorLoading:865609025197834240>";
 
         public static readonly string NoData = "<:NoData:865624964044292096>";
+
+        public static readonly string Check = "<:Check:874023090546442251>";
+
+        public static readonly string UnCheck = "<:UnCheck:874023093125914654>";
     }
 }
--- DataProcessor/RaidManager/Raid.cs
+++ DataProcessor/RaidManager/Raid.cs
@@ -1,15 +1,18 @@
-﻿namespace DataProcessor.RaidManager
+﻿using static DataProcessor.RaidManager.RaidType;
+
+namespace DataProcessor.RaidManager
 {
     class Raid
     {
         public static RaidType GetRaidType(long hash) =>
             hash switch
             {
-                1661734046 or 2122313384 or 2214608156 or 2214608157 => RaidType.LW,
-                3458480158 or 2497200493 or 2659723068 or 3845997235 => RaidType.GOS,
-                910380154 or 3976949817 => RaidType.DSC,
-                3881495763 => RaidType.VOG_L,
-                1681562271 => RaidType.VOG_M
+                1661734046 or 2122313384 or 2214608156 or 2214608157 => LW,
+                3458480158 or 2497200493 or 2659723068 or 3845997235 => GOS,
+                910380154 or 3976949817 => DSC,
+                3881495763 => VOG_L,
+                1681562271 => VOG_M,
+                _ => Undefined
             };
     }
 }
