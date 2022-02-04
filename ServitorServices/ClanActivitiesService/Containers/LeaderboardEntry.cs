2f70d05e1939b5e29e9fc5527773c6071691f394
Віталій Вдовенко <taybenberg@gmail.com>
2022-02-04 03:11:04 +0200

    Code cleanup

--- ServitorServices/ClanActivitiesService/Containers/LeaderboardEntry.cs
+++ ServitorServices/ClanActivitiesService/Containers/LeaderboardEntry.cs
@@ -5,13 +5,13 @@ namespace ClanActivitiesService.Containers
     public record LeaderboardEntry
     {
         public bool IsCurrUser { get; internal set; }
-        
+
         public int Rank { get; internal set; }
 
         public DestinyClass DestinyClass { get; internal set; }
 
         public string UserName { get; internal set; }
-        
+
         public string Value { get; internal set; }
     }
 }
