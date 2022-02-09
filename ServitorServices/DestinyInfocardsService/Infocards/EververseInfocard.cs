fb9b8c468b0264bcd5fc14c8674f30ca0e91ba36
Віталій Вдовенко <taybenberg@gmail.com>
2022-02-09 03:23:39 +0200

    Code cleanup

--- ServitorServices/DestinyInfocardsService/Infocards/EververseInfocard.cs
+++ ServitorServices/DestinyInfocardsService/Infocards/EververseInfocard.cs
@@ -1,9 +1,9 @@
 ﻿namespace DestinyInfocardsService.Infocards
 {
     public record EververseInfocard
-    { 
+    {
         public int WeekNumber { get; internal set; }
-   
+
         public DateTime ResetBegin { get; internal set; }
 
         public DateTime ResetEnd { get; internal set; }
